7887e6ed25e65bd35c574297a3c94f666b663f9d
Dannynsikak <nsikakdanny11@gmail.com>
2024-11-29 17:50:52 +0100

    performed CRUD operations and authentication using Deno and deno middlewares

---
+++ middleware/mod.ts
@@ -0,0 +1,6 @@
+import login from "./login.ts";
+import register from "./register.ts";
+import updateAccount from "./auth/updateAccounts.ts";
+import deleteAccount from "./auth/deleteAccounts.ts";
+
+export { login, register, updateAccount, deleteAccount };
